bb3e69fe5eddc05311143b67f612156e1ea00d94
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2025-01-19 17:16:27 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,17 @@
 export const Card = (props) => {
+  var { id, img_url, name, rating, genre, description, watch_url } = props.elem;
+
   return (
-    <div className="series" key={props.elem.id}>
+    <div className="series" key={id}>
       <div className="poster">
-        <img src={props.elem.img_url} alt="" width="50%" />
+        <img src={img_url} alt="" width="50%" />
       </div>
-      <h1>Name : {props.elem.name} </h1>
-      <h1>Rating: {props.elem.rating}</h1>
-      <h1>Genre : {props.elem.genre}</h1>
+      <h1>Name : {name} </h1>
+      <h1>Rating: {rating}</h1>
+      <h1>Genre : {genre}</h1>
       <h1>Summary :</h1>
-      <p>{props.elem.description}</p>
-      <a href={props.elem.watch_url}>
+      <p>{description}</p>
+      <a href={watch_url}>
         <button>Watch Now </button>
       </a>
     </div>
